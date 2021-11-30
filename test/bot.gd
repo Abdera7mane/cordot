007d63a5ee1af9a2d81b6dc6168ae8b725cd9d39
Abdera7mane <56tzminecraft@gmail.com>
2021-11-30 23:30:03 +0100

    Update example bot

--- test/bot.gd
+++ test/bot.gd
@@ -1,19 +1,20 @@
 extends Node
 
+# warning-ignore-all:return_value_discarded
+
 var cmd_arg: Dictionary
-
 var client: DiscordClient
-
-var something: int
 
 func _init() -> void:
 	parse_cmdline_args()
 
 func _ready() -> void:
 	client = DiscordClient.new(get_token(), GatewayIntents.ALL)
-	# warning-ignore:return_value_discarded
-	client.connect("client_ready", self, "_on_ready")
 	add_child(client)
+	
+	client.connect("client_ready", self, "_on_ready")
+	client.connect("message_sent", self, "_on_message")
+	
 	client.login()
 
 func parse_cmdline_args() -> void:
@@ -31,3 +32,11 @@ func get_token() -> String:
 func _on_ready(user: User) -> void:
 	print("Bot is ready !")
 	print("logged as: ", user.username)
+
+func _on_message(message: Message) -> void:
+	if message.content == "?ping":
+		var start: int = OS.get_ticks_msec()
+		var content = "**Gateway ping**: %d" % client.get_ping()
+		var message_sent: Message = yield(message.channel.send_message(content), "completed")
+		content = content + ", **REST ping**: %d" % (OS.get_ticks_msec() - start)
+		yield(message_sent.edit(content), "completed")

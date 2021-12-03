64a446b8a63518a9a870c4c153a656ff7a4e0f4a
Abdera7mane <56tzminecraft@gmail.com>
2021-12-03 17:05:43 +0100

    Add type property to PartialChannel

--- addons/Giscord/entity/channel/PartialChannel.gd
+++ addons/Giscord/entity/channel/PartialChannel.gd
@@ -1,12 +1,18 @@
 class_name PartialChannel extends DiscordEntity
 
 var name: String setget __set
+var type: int    setget __set
 
-func _init(id: int, _name: String).(id) -> void:
-	name = _name
+func _init(data: Dictionary).(data["id"]) -> void:
+	name = data["name"]
+	type = data.get("type", Channel.Type.UNKNOWN)
+	
 
 func is_partial() -> bool:
 	return true
+
+func get_class() -> String:
+	return "PartialChannel"
 
 func __set(_value) -> void:
 	pass

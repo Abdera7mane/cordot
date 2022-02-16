642b7bc42c7952a6378bfac7453448d878e5b054
Abdera7mane <59313921+Abdera7mane@users.noreply.github.com>
2022-02-16 04:48:34 +0100

    Check for name equality only in UnicodeEmoji

--- addons/Giscord/entity/emoji/UnicodeEmoji.gd
+++ addons/Giscord/entity/emoji/UnicodeEmoji.gd
@@ -3,6 +3,9 @@ class_name UnicodeEmoji extends Emoji
 func _init(unicode: String).({id = 0, name = unicode}):
 	pass
 
+func equals(entity: DiscordEntity) -> bool:
+	return .equals(entity) and name == entity.name
+
 func url_encoded() -> String:
 	return name.http_escape()
 

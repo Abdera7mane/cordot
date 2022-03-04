ad200345fd46ee3c0e91b8502b16a8fa2abfa4db
Abdera7mane <59313921+Abdera7mane@users.noreply.github.com>
2022-03-04 21:59:40 +0100

    Set Discord API version to V9

--- addons/Giscord/Discord.gd
+++ addons/Giscord/Discord.gd
@@ -1,10 +1,10 @@
 class_name Discord
 
 const REST_URL: String = "https://discord.com/api/v%d"
-const REST_VERSION: int = 8
+const REST_VERSION: int = 9
 const CDN_URL: String = "https://cdn.discordapp.com"
 const GATEWAY_URL: String = "wss://gateway.discord.gg/?v=%d&encoding=%s"
-const GATEWAY_VERSION: int = 8
+const GATEWAY_VERSION: int = 9
 const VOICE_GATEWAY_VERSION: int = 4
 
 const LIBRARY: String = "Giscord"

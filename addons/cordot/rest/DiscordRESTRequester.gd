514f467450dab26d45f6da4d4d7189a563da9b0c
Abdera7mane <59313921+Abdera7mane@users.noreply.github.com>
2022-07-06 04:28:41 +0100

    Better REST API error debugging

--- addons/cordot/rest/DiscordRESTRequester.gd
+++ addons/cordot/rest/DiscordRESTRequester.gd
@@ -8,20 +8,38 @@ func _init(use_pool: bool = false) -> void:
 func request_async(request: RestRequest) -> HTTPResponse:
 	var response: HTTPResponse = yield(limiter.queue_request(request), "completed")
 	
+	var failed: bool = false
+	
 	match response.code:
 		HTTPClient.RESPONSE_BAD_REQUEST:
-			push_error("Discord REST: Bad Request, url: %s" % request.url)
+			failed = true
+			print_error("The request was improperly formatted, or the server couldn't understand it", request)
 		HTTPClient.RESPONSE_UNAUTHORIZED:
-			push_error("Discord REST: Unauthorized, url: %s" % request.url)
+			failed = true
+			print_error("The Authorization header is missing or invalid", request)
 		HTTPClient.RESPONSE_FORBIDDEN:
-			push_error("Discord REST: Forbidden, url: %s" % request.url)
-			print(response.body.get_string_from_utf8())
+			failed = true
+			print_error("The Authorization token did not have permission to the resource", request)
 		HTTPClient.RESPONSE_NOT_FOUND:
-			push_error("Discord REST: Not Found, url: %s" % request.url)
+			failed = true
+			print_error("The resource at the location specified doesn't exist", request)
 		HTTPClient.RESPONSE_METHOD_NOT_ALLOWED:
-			push_error("Discord REST: Method Not Allowed, url: %s, method: %d" % [request.url, request.method])
-		HTTPClient.RESPONSE_TOO_MANY_REQUESTS:
-			push_error("Discord REST: Rate Limited, url: %s" % request.url)
+			failed = true
+			print_error("The HTTP method used is not valid for the location specified", request)
+		HTTPClient.RESPONSE_BAD_GATEWAY: # GATEWAY UNAVAILABLE
+			failed = true
+			print_error("There was not a gateway available to process the request. Wait a bit and retry", request)
+		var code:
+			failed = code >= 500
+			if failed:
+				print_error("Server error when processing the request", request)
+	if failed:
+		var parse_result := JSON.parse(response.body.get_string_from_utf8().c_unescape())
+		if parse_result.error == OK and parse_result.result is Dictionary:
+			var error_object: Dictionary = parse_result.result
+			if error_object.has("code"):
+				printerr()
+				print_error_object(error_object)
 	
 	return response
 
@@ -65,6 +83,50 @@ func cdn_download_async(_url: String) -> Resource:
 
 func get_last_latency_ms() -> int:
 	return limiter.last_latency_ms
+
+func print_error(message: String, request: RestRequest) -> void:
+	push_error("Discord REST: "+ message)
+	printerr("Discord REST Error")
+	printerr("message: ", message)
+	printerr("URL: ", request.url)
+	printerr("Method: ", limiter._stringify_method(request.method))
+	printerr("Body length: ", request.body.size())
+
+func print_error_object(object: Dictionary) -> void:
+	printerr("ERROR TRACE START")
+	printerr()
+	printerr("Error: %s (%s)" % [object["code"], object["message"]])
+	
+	if not object.has("errors"):
+		printerr()
+		printerr("ERROR TRACE END")
+		return
+	
+	printerr("Errors:")
+	
+	var stack: Array = [object["errors"]]
+	while stack:
+		var dict: Dictionary = stack.pop_back()
+		for key in  dict:
+			var value = dict[key]
+			if value is Dictionary:
+				var path: String = dict.get("path", "")
+				if key.is_valid_integer():
+					path += "[%s]" % key
+				else:
+					path += "." + key
+				value["path"] = path.lstrip(".")
+				stack.append(value)
+		if dict.has("_errors"):
+			var path: String = dict.get("path", "")
+			var tabbing: String
+			if not path.empty():
+				tabbing = "\t"
+				printerr("\t%s:" % path)
+			for error in dict["_errors"]:
+				printerr(tabbing + "\t\u2022 %s: %s" % [error["code"], error["message"]])
+	printerr()
+	printerr("ERROR TRACE END")
 
 static func _power_of_2(n: int) -> bool:
 	return (n & (n - 1)) == 0 if n > 0 else false
